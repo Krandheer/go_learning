9179ba396d2b222071452050d9d276413088c662
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-26 18:23:54 +0530

    loops in go

--- loops/main.go
+++ loops/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("for loop")
+	fmt.Println("welcome to loop in golang")
+	var days = []string{"sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"}
+	fmt.Println(days)
+
+	// for i := 0; i < len(days); i++ {
+	// 	fmt.Println(days[i])
+	// }
+
+	// for v := range days {
+	// 	fmt.Println(days[v])
+	// }
+
+	for index, v := range days {
+		fmt.Printf("index is %v and day is %v\n", index, v)
+	}
 }
