9b8d9d803bec629d7ed5afc5244f612d0121ef58
randheer <randheer.dakshana15@gmail.com>
2024-07-21 16:38:02 +0530

    sync module, mutex and wait group learned

--- cmd/learning_2/main.go
+++ cmd/learning_2/main.go
@@ -3,25 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
+var wg sync.WaitGroup
+var mut sync.Mutex
+
+var signals = []string{"test"}
 func main() {
-	fmt.Println("testing few things")
+// learning wait group and mutex 
 	websitelist := []string{
-		"https://lco.dev", 
 		"https://go.dev", 
 		"https://google.com", 
+		"https://facebook.com", 
+		"https://github.com", 
+		"https://go.dev", 
+		"https://google.com", 
+		"https://facebook.com", 
+		"https://github.com", 
 	}
 	for _, web := range websitelist {
-		getStatusCode(web)
+		go getStatusCode(web)
+		wg.Add(1)
 	}
+	wg.Wait()
+	fmt.Println(signals)
 }
 
 func getStatusCode(endpoint string) {
+	defer wg.Done()
+
 	res, err := http.Get(endpoint)
 	if err != nil {
 		fmt.Println("error occured", err)
 	} else {
+		mut.Lock()
+		signals = append(signals, endpoint)
+		mut.Unlock()
 		fmt.Printf("%d of the this %s website \n", res.StatusCode, endpoint)
 	}
 }
