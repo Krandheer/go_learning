d767e4f010fa86a86ee88875f344444ddcab4be3
randheer <randheer.dakshana15@gmail.com>
2025-07-05 19:53:13 +0530

    go

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module go_learning
 
-go 1.22.5
+go 1.23.0
+
+toolchain go1.23.8
+
+require (
+	google.golang.org/genproto v0.0.0-20250428153025-10db94c68c34 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-var count int = 0
-var mu sync.Mutex
-var wg sync.WaitGroup
-
-func increment() {
-	mu.Lock()
-	defer mu.Unlock()
-	count += 1
-	wg.Done()
-
-}
+import cmd "go_learning/cmd/learning_1"
 
 func main() {
-	for range 1000000 {
-		wg.Add(1)
-		go increment()
-	}
-	wg.Wait()
-	fmt.Println(count)
+	cmd.BenchMarking()
 }
