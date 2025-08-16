1c5b49268ce8d1c60ec4eee6b4a523c98fc1f41b
randheer <randheer.dakshana15@gmail.com>
2025-08-16 20:58:03 +0530

    reading file in go

--- cmd/learning_1/readFile.go
+++ cmd/learning_1/readFile.go
@@ -9,59 +9,59 @@ import (
 )
 
 func getLinesChannel(f io.Reader) <-chan string {
-    out := make(chan string, 1)
+	out := make(chan string, 1)
 
-    go func() {
-        defer close(out)
-        var buf bytes.Buffer
-        chunk := make([]byte, 8)
+	go func() {
+		defer close(out)
+		var buf bytes.Buffer
+		chunk := make([]byte, 8)
 
-        for {
-            n, err := f.Read(chunk)
+		for {
+			n, err := f.Read(chunk)
 
-            if n > 0 {
-                data := chunk[:n]
-                for {
-                    if i := bytes.IndexByte(data, '\n'); i >= 0 {
-                        buf.Write(data[:i])
-                        out <- buf.String() 
-                        buf.Reset()
+			if n > 0 {
+				data := chunk[:n]
+				for {
+					if i := bytes.IndexByte(data, '\n'); i >= 0 {
+						buf.Write(data[:i])
+						out <- buf.String()
+						buf.Reset()
 
-                        if i+1 < len(data) {
-                            data = data[i+1:]
-                            continue
-                        }
-                        break 
-                    }
-                    buf.Write(data)
-                    break
-                }
-            }
+						if i+1 < len(data) {
+							data = data[i+1:]
+							continue
+						}
+						break
+					}
+					buf.Write(data)
+					break
+				}
+			}
 
-            if err != nil {
-                if errors.Is(err, io.EOF) {
-                    if buf.Len() > 0 {
-                        out <- buf.String()
-                    }
-                    return 
-                }
-                return
-            }
-        }
-    }()
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					if buf.Len() > 0 {
+						out <- buf.String()
+					}
+					return
+				}
+				return
+			}
+		}
+	}()
 
-    return out 
+	return out
 }
 
 func ReadByte() {
-    f, err := os.Open("hello.txt")
-    if err != nil {
-        panic(err)
-    }
+	f, err := os.Open("hello.txt")
+	if err != nil {
+		panic(err)
+	}
 	defer f.Close()
-    lines := getLinesChannel(f)
+	lines := getLinesChannel(f)
 
-    for line := range lines {
-        fmt.Println("Line:", line)
-    }
+	for line := range lines {
+		fmt.Println("Line:", line)
+	}
 }
